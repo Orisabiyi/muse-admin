59e8eab1bca9767a342b6776280a1cde8475f8b6
orisabiyi <orisabiyidavid@gmail.com>
2026-02-06 09:33:35 +0100

    feat: rename useProducts to useGetAllProducts and add useGetProductById hook

--- hooks/useProducts.ts
+++ hooks/useProducts.ts
@@ -1,9 +1,16 @@
 import { productApi } from "@/lib/api"
 import { useQuery } from "@tanstack/react-query"
 
-export const useProducts = (page: number = 1, limit: number = 10, search: string) => {
+export const useGetAllProducts = (page: number = 1, limit: number = 10, search: string) => {
   return useQuery({
     queryKey: ["products", page, limit, search],
     queryFn: async () => productApi.getAllProducts(page, limit, search),
+  })
+}
+
+export const useGetProductById = (id: string) => {
+  return useQuery({
+    queryKey: ["product", id],
+    queryFn: async () => productApi.getProductById(id),
   })
 }
